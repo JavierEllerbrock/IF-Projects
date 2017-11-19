b32c479b1c71b4cb4fb400083e7607de48a751c2
JavierEllerbrock <33645714+JavierEllerbrock@users.noreply.github.com>
2017-11-18 18:01:10 -0800

    Add files via upload
    In this project it is determined if the number the user inserted is either positive, negative or zero

---
+++ Exercise3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num;
+
+            Console.Write("Please insert a number: ");
+            num = decimal.Parse(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("The number is negative ");
+            }
+
+            else if (num > 0)
+            {
+                Console.WriteLine("The number is positive");
+            }
+
+            else
+            {
+                Console.WriteLine("The number is zero");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
