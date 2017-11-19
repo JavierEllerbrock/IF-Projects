9734b886f6c6f3457280aa3e4ddeedef39b8d619
JavierEllerbrock <33645714+JavierEllerbrock@users.noreply.github.com>
2017-11-18 17:46:31 -0800

    Add files via upload
    In this project it will be told what is the status of a person regarding the age

---
+++ Exercise2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            Console.Write("Please insert your age: ");
+            age = int.Parse(Console.ReadLine());
+
+            if (0 < age && age < 2)
+            {
+                Console.WriteLine("You are a baby");
+            }
+
+            else if (2 <= age && age < 4)
+            {
+                Console.WriteLine("You are a toddler");
+            }
+
+            else if (4 <= age && age < 12)
+            {
+                Console.WriteLine("You are a child");
+            }
+
+            else if (12 <= age && age < 21)
+            {
+                Console.WriteLine("You are a teenager (acording to U.S law)");
+            }
+
+            else if (21 <= age && age < 50)
+            {
+                Console.WriteLine("You are an adult");
+            }
+
+            else if (age >= 50)
+            {
+                Console.WriteLine("You are a senior");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
